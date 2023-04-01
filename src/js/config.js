1360515870373dc7bc966444448e96d69d08c45e
sevvalbugdayci <sevvalbugdayci@gmail.com>
2023-04-02 00:49:29 +0300

    movie app

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const api_key = "7441423270de5dc1ca56ea74c1135000";
+export const base_url = "https://api.themoviedb.org/3/";
---
+++ src/js/models/Movie.js
@@ -0,0 +1,12 @@
+import { base_url,api_key } from "../config";
+
+export class Movie {
+    constructor(id) {
+        this.id = id;
+    }
+
+    async getMovie() {
+        const response = await fetch(`${base_url}/movie/${this.id}?api_key=${api_key}`);
+        this.data = await response.json();
+    }
+}
